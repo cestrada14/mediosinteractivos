37edb5f796f9723665855a5c4975384d4f94fd05
cestrada14 <31419438+cestrada14@users.noreply.github.com>
2017-10-22 20:57:05 -0500

    Update sketch.js

--- 03/sketch.js
+++ 03/sketch.js
@@ -1,3 +1,7 @@
+var miCancion;
+var miAmplitud;
+var comienzo;
+
 var x = 0;
 var y = 0;
 var dirX= 1;
@@ -58,9 +62,17 @@ var rojo = 49;
 var verde = 211;
 var azul = 77;
 
+function preload() {
+  miCancion = loadSound('Assets/069 - akira ifukube - godzilla - main title (1954).mp3');
+}
+
 function setup() { 
   createCanvas(400, 400); 
   var millisecond = millis();
+  miCancion.setVolume(0.3);
+  miCancion.play();
+  comienzo = millis();
+  print(comienzo);
 } 
 
 function draw() { 
@@ -78,35 +90,35 @@ function draw() {
   
 //Circulos
   
-  if(millis() > 1000 && millis() < 1250) {
+  if(millis() > (comienzo + 1000) && millis() < (comienzo + 1250)) {
   fill(248, 133, 6);
   ellipse(x1, 200, diametro1, diametro1);
   x1 = x1 - 50
   velX = 1;
   }
  
-  if(millis() > 1250 && millis() < 1500) {
+  if(millis() > (comienzo + 1250) && millis() < (comienzo + 1500)) {
   fill(248, 133, 6);
   ellipse(200, y1, diametro1, diametro1);
   y1 = y1 - 50
   velX = 1;
   }
   
-  if(millis() > 1500 && millis () < 1750) {
+  if(millis() > (comienzo + 1500) && millis () < (comienzo + 1750)) {
   fill(248, 133, 6);
   ellipse(x2, 200, diametro1, diametro1);
   x2 = x2 + 50;
   velX = 1;
   }
   
-  if(millis() > 1750 && millis() < 2000) {
+  if(millis() > (comienzo + 1750) && millis() < (comienzo + 2000)) {
   fill(248, 133, 6);
   ellipse(200, y2, diametro1, diametro1);
   y2 = y2 + 50;
   velX = 1;
   }
   
-  if(millis() > 2000 && millis() < 2250) {
+  if(millis() > (comienzo + 2000) && millis() < (comienzo + 2250)) {
   fill(248, 133, 6);
   ellipse(x3, y3, diametro1, diametro1);
   x3 = x3 - 50;
@@ -114,7 +126,7 @@ function draw() {
   velX = 1;
   }
   
-  if(millis() > 2250 && millis() < 2500) {
+  if(millis() > (comienzo + 2250) && millis() < (comienzo + 2500)) {
   fill(248, 133, 6);
   ellipse(x4, y4, diametro1, diametro1);
   x4 = x4 + 50;
@@ -122,7 +134,7 @@ function draw() {
   velX = 1;
   }
   
-  if(millis() > 2500 && millis() < 2750) {
+  if(millis() > (comienzo + 2500) && millis() < (comienzo + 2750)) {
   fill(248, 133, 6);
   ellipse(x5, y5, diametro1, diametro1);
   x5 = x5 + 50;
@@ -130,7 +142,7 @@ function draw() {
   velX = 1;
   }
   
-  if(millis() > 2750 && millis() < 3000) {
+  if(millis() > (comienzo + 2750) && millis() < (comienzo + 3000)) {
   fill(248, 133, 6);
   ellipse(x6, y6, diametro1, diametro1);
   x6 = x6 - 50;
@@ -140,28 +152,28 @@ function draw() {
   
 //Rectangulos rojos triqui
   
-  if(millis() > 3500 && millis() < 11000) {
+  if(millis() > (comienzo + 3500) && millis() < (comienzo + 11000)) {
   fill(250, 0, 0);
   rect(100, 0, 20, largo1);
   largo1 = largo1 + 20;
   velY = 20; 
   } 
   
-  if(millis() > 4500 && millis() < 11000) {
+  if(millis() > (comienzo + 4500) && millis() < (comienzo + 11000)) {
   fill(250, 0, 0);
   rect(280, 0, 20, largo2);
   largo2 = largo2 + 20;
   velY = 20;
   } 
   
-  if(millis() > 5500 && millis() < 11000) {
+  if(millis() > (comienzo + 5500) && millis() < (comienzo + 11000)) {
   fill(250, 0, 0);
   rect(0, 100, ancho1, 20);
   ancho1 = ancho1 + 20;
   velY = 20;
   } 
   
-  if(millis() > 6500 && millis() < 11000) {
+  if(millis() > (comienzo + 6500) && millis() < (comienzo + 11000)) {
   fill(250, 0, 0);
   rect(0, 280, ancho2, 20);
   ancho2 = ancho2 + 20;
@@ -170,28 +182,28 @@ function draw() {
   
 //Rectangulos rosados triqui
   
-  if(millis() > 7500 && millis() < 11000) {
+  if(millis() > (comienzo + 7500) && millis() < (comienzo + 11000)) {
   fill(245, 28, 136);
   rect(50, 0, 20, largo1);
   largo1 = largo1 + 20;
   velY = 20; 
   } 
   
-  if(millis() > 8000 && millis() < 11000) {
+  if(millis() > (comienzo + 8000) && millis() < (comienzo + 11000)) {
   fill(245, 28, 136);
   rect(330, 0, 20, largo2);
   largo2 = largo2 + 20;
   velY = 20;
   } 
   
-  if(millis() > 8500 && millis() < 11000) {
+  if(millis() > (comienzo + 8500) && millis() < (comienzo + 11000)) {
   fill(245, 28, 136);
   rect(0, 50, ancho1, 20);
   ancho1 = ancho1 + 20;
   velY = 20;
   } 
   
-  if(millis() > 9500 && millis() < 11000) {
+  if(millis() > (comienzo + 9500) && millis() < (comienzo + 11000)) {
   fill(245, 28, 136);
   rect(0, 330, ancho2, 20);
   ancho2 = ancho2 + 20;
@@ -200,51 +212,51 @@ function draw() {
   
  //Circulos 
 
-  if(millis() > 11000 && millis() < 14000) {
+  if(millis() > (comienzo + 11000) && millis() < (comienzo + 14000)) {
   fill(246,68, 90);
   ellipse(200, 200, diametro2, diametro2);
   }
   
-  if(millis() > 11750 && millis() < 14000) {
+  if(millis() > (comienzo + 11750) && millis() < (comienzo + 14000)) {
   fill(236,58, 80);
   ellipse(200, 200, diametro3, diametro3);
   } 
   
-  if(millis() > 12500 && millis() < 14000) {
+  if(millis() > (comienzo + 12500) && millis() < (comienzo + 14000)) {
   fill(226, 48, 70);
   ellipse(200, 200, diametro4, diametro4);
   } 
   
-  if(millis() > 13250 && millis() < 14000) {
+  if(millis() > (comienzo + 13250) && millis() < (comienzo + 14000)) {
   fill(216, 38, 60);
   ellipse(200, 200, diametro5, diametro5);
   } 
 
 //Triangulos
   
-  if(millis() > 14250 && millis() < 16750) {
+  if(millis() > (comienzo + 14250) && millis() < (comienzo + 16750)) {
   fill(121, 79, 260);
   triangle(390, 300, 390, 390, 300, 300);
   }
 
-  if(millis() > 15000 && millis() < 16750) {
+  if(millis() > (comienzo + 15000) && millis() < (comienzo + 16750)) {
   fill(111, 69, 250);
   triangle(300, 390, 300, 300, 390, 390);
   } 
   
-  if(millis() > 15750 && millis() < 16750) {
+  if(millis() > (comienzo + 15750) && millis() < (comienzo + 16750)) {
   fill(101, 59, 230);
   triangle(300, 300, 345, 250, 390, 300);
   } 
   
-  if(millis() > 16500 && millis() < 16750) {
+  if(millis() > (comienzo + 16500) && millis() < (comienzo + 16750)) {
   fill(71, 29, 200);
   triangle(300, 300, 300, 390, 250, 350);
   } 
   
 // Circulo
   
-  if(millis() > 17000 && millis() < 18500) {
+  if(millis() > (comienzo + 17000) && millis() < (comienzo + 18500)) {
   fill(21, 9, 120);
   ellipse(200, 200, diametro6, diametro6);
   diametro6 = diametro6 - 4;
@@ -253,7 +265,7 @@ function draw() {
   
 //Rombos
   
-  if(millis() > 18750 && millis() < 26000) {
+  if(millis() > (comienzo + 18750) && millis() < (comienzo + 26000)) {
   fill(56, 163, 249);
   beginShape();
   vertex(50, 10);
@@ -263,7 +275,7 @@ function draw() {
   endShape(CLOSE);
   }
   
-  if(millis() > 19750 && millis() < 26000) {
+  if(millis() > (comienzo + 19750) && millis() < (comienzo + 26000)) {
   fill(46, 153, 239);
   beginShape();
   vertex(50, 90);
@@ -273,7 +285,7 @@ function draw() {
   endShape(CLOSE);
   }
   
-  if(millis() > 20500 && millis() < 26000) {
+  if(millis() > (comienzo + 20500) && millis() < (comienzo + 26000)) {
   fill(26, 143, 229);
   beginShape();
   vertex(50, 90);
@@ -283,7 +295,7 @@ function draw() {
   endShape(CLOSE);
   }
   
-  if(millis() > 21250 && millis() < 26000) {
+  if(millis() > (comienzo + 21250) && millis() < (comienzo + 26000)) {
   fill(6, 126, 209);
   beginShape();
   vertex(50, 170);
@@ -293,7 +305,7 @@ function draw() {
   endShape(CLOSE);
   }
   
-  if(millis() > 22500 && millis() < 26000) {
+  if(millis() > (comienzo + 22500) && millis() < (comienzo + 26000)) {
   fill(0, 116, 199);
   beginShape();
   vertex(50, 170);
@@ -303,7 +315,7 @@ function draw() {
   endShape(CLOSE);
   }
   
-  if(millis() > 23250 && millis() < 26000) {
+  if(millis() > (comienzo + 23250) && millis() < (comienzo + 26000)) {
   fill(0, 96, 179);
   beginShape();
   vertex(80, 210);
@@ -313,7 +325,7 @@ function draw() {
   endShape(CLOSE);
   }
   
-  if(millis() > 23750 && millis() < 26000) {
+  if(millis() > (comienzo + 23750) && millis() < (comienzo + 26000)) {
   fill(0, 86, 169);
   beginShape();
   vertex(50, 250);
@@ -323,7 +335,7 @@ function draw() {
   endShape(CLOSE);
   }
   
-  if(millis() > 24500 && millis() < 26000) {
+  if(millis() > (comienzo + 24500) && millis() < (comienzo + 26000)) {
   fill(0, 76, 159);
   beginShape();
   vertex(80, 290);
@@ -335,18 +347,18 @@ function draw() {
     
 //Cuadrado que se mueve
   
-  if(millis() > 26000 && millis() < 29190) {
+  if(millis() > (comienzo +  26000) && millis() < (comienzo +  29190)) {
   noStroke();
   fill(rojo, verde, azul);
   rect(x7, y7, 100, 100);
   x7 = x7 + (velX2 * dirX);
   }
   
-  if(x7 < 0 && x7 > 340) {
+  if(x7 <  0 && x7 > 340) {
   velX2 = 20;
   }
   
-  if(x7 < 0 || x7 > 340) {
+  if(x7 < + 0 || x7 > 340) {
   rect(x7, y7, 50, 50);
   dirX = -dirX;
   rojo = rojo + 50;
@@ -360,40 +372,40 @@ function draw() {
   
 //Cuadros que suben
   
-  if(millis() > 29190 && millis() < 34000) {
+  if(millis() > (comienzo + 29190) && millis() < (comienzo + 34000)) {
   noStroke();
   fill(34, 149, 78);
   rect(0, 300, 400, 100);
   }
   
-  if(millis() > 30000 && millis() < 34000) {
+  if(millis() > (comienzo + 30000) && millis() < (comienzo + 34000)) {
   fill(14, 129, 58);
   rect(0, 300, 100, 100);
   }
   
-  if(millis() > 30000 && millis() < 34000) {
+  if(millis() > (comienzo + 30000) && millis() < (comienzo + 34000)) {
   fill(74, 179, 108);
   rect(0, y8, 100, 100);
   y8 = y8 - 10;
   }
   
-  if(millis() > 31000 && millis() < 34000) {
+  if(millis() > (comienzo + 31000) && millis() < (comienzo + 34000)) {
   fill(14, 129, 58);
   rect(300, 300, 100, 100);
   }
   
-  if(millis() > 31000 && millis() < 34000) {
+  if(millis() > (comienzo + 31000) && millis() < (comienzo + 34000)) {
   fill(74, 179, 108);
   rect(300, y9, 200, 100);
   y9 = y9 - 10;
   }
   
-  if(millis() > 32000 && millis() < 34000) {
+  if(millis() > (comienzo + 32000) && millis() < (comienzo + 34000)) {
   fill(14, 129, 58);
   rect(100, 300, 200, 100);
   }
   
-  if(millis() > 32000 && millis() < 34000) {
+  if(millis() > (comienzo + 32000) && millis() < (comienzo + 34000)) {
   fill(74, 179, 108);
   rect(100, y10, tamaño2, tamaño3);
   y10 = y10 - 3;
@@ -403,22 +415,22 @@ function draw() {
   
 //Lineas diagonales
   
-  if(millis() > 34000 && millis() < 38000) {
+  if(millis() > (comienzo + 34000) && millis() < (comienzo + 38000)) {
   stroke(0, 0, 0);
   strokeWeight(10);
   fill(0, 0,0);
   line(0, 50, 350, 400);
   }
   
-  if(millis() > 35000 && millis() < 38000) {
+  if(millis() > (comienzo + 35000) && millis() < (comienzo + 38000)) {
   line(0, 0, 400, 400);
   }
   
-  if(millis() > 36000 && millis() < 38000) {
+  if(millis() > (comienzo + 36000) && millis() < (comienzo + 38000)) {
   line(50, 0, 400, 350);
   }
   
-  if(millis() > 37250 && millis() < 38000) {
+  if(millis() > (comienzo + 37250) && millis() < (comienzo + 38000)) {
   stroke(250, 250, 250);
   line(400, 0, x8, y11);
   x8 = 0
@@ -427,9 +439,11 @@ function draw() {
   
 //Circulos 
   
-  if(millis() > 38000 && millis() < 38200 || millis() > 38400 && millis() < 38600 || 
-  millis() > 38800 && millis() < 39000 || millis() > 39200 && millis() < 39400 || 
-  millis() > 39600 && millis () < 39800) {
+  if(millis() > (comienzo + 38000) && millis() < (comienzo + 38200) || 
+  millis() > (comienzo + 38400) && millis() < (comienzo + 38600) || 
+  millis() > (comienzo + 38800) && millis() < (comienzo + 39000) || 
+  millis() > (comienzo + 39200) && millis() < (comienzo + 39400) || 
+  millis() > (comienzo + 39600) && millis() < (comienzo + 39800)) {
   noStroke();
   fill(251, 197, 52, 100);
   ellipse(150, 150, 200, 200);
@@ -439,12 +453,13 @@ function draw() {
   ellipse(200, 250, 200, 200);
   }
   
-  if(millis() > 39800 && millis() < 40000) {
+  if(millis() > (comienzo + 39800) && millis() < (comienzo + 40000)) {
   fill(152, 108, 57);
   ellipse (200, 190, 20, 20);
   }
   
-  if(millis() > 40000 && millis() < 40200 || millis() > 40400 && millis() < 40600) {
+  if(millis() > (comienzo + 40000) && millis() < (comienzo + 40200) || 
+  millis() > (comienzo + 40400) && millis() < (comienzo + 40600)) {
   noStroke();
   fill(251, 197, 52, 100);
   ellipse(150, 150, 200, 200);
@@ -454,13 +469,15 @@ function draw() {
   ellipse(200, 250, 200, 200);
   }
   
-  if(millis() > 40800 && millis() < 41000) {
+  if(millis() > (comienzo + 40800) && millis() < (comienzo + 41000)) {
   fill(152, 108, 57);
   ellipse (200, 190, 20, 20);
   }
   
-  if(millis() > 41000 && millis() < 41200 || millis() > 41400 && millis() < 41600 || 
-  millis() > 41800 && millis() < 42000 || millis() > 42200 && millis() < 42400) {
+  if(millis() > (comienzo + 41000) && millis() < (comienzo + 41200) || 
+  millis() > (comienzo + 41400) && millis() < (comienzo + 41600) || 
+  millis() > (comienzo + 41800) && millis() < (comienzo + 42000) || 
+  millis() > (comienzo + 42200) && millis() < (comienzo + 42400)) {
   noStroke();
   fill(251, 197, 52, 100);
   ellipse(150, 150, 200, 200);
@@ -470,15 +487,18 @@ function draw() {
   ellipse(200, 250, 200, 200);
   }
   
-  if(millis() > 42400 && millis() < 42600) {
+  if(millis() > (comienzo + 42400) && millis() < (comienzo + 42600)) {
   fill(152, 108, 57);
   ellipse (200, 190, 20, 20);
   }
   
-  if(millis() > 42600 && millis() < 42800 || millis() > 43000 && millis() < 43200 || 
-  millis() > 43400 && millis() < 43600 || millis() > 43800 && millis() < 44000 || 
-  millis() > 44200 && millis() < 44400 || millis() > 44600 && millis() < 44800 || 
-  millis() > 45000 && millis() < 45200 || millis() > 45400 && millis() < 45600) {
+  if(millis() > (comienzo + 42600) && millis() < (comienzo + 42800) || 
+  millis() > (comienzo + 43000) && millis() < (comienzo + 43200) || 
+  millis() > (comienzo + 43400) && millis() < (comienzo + 43600) || 
+  millis() > (comienzo + 43800) && millis() < (comienzo + 44000) || 
+  millis() > (comienzo + 44200) && millis() < (comienzo + 44400) || 
+  millis() > (comienzo + 44600) && millis() < (comienzo + 44800) || 
+  millis() > (comienzo + 45000) && millis() < (comienzo + 45200)) {
   noStroke();
   fill(251, 197, 52, 100);
   ellipse(150, 150, 200, 200);
@@ -490,24 +510,24 @@ function draw() {
  
 //Rectanguos
   
-  if(millis() > 45600 && millis() < 51000) {
+  if(millis() > (comienzo + 45600) && millis() < (comienzo + 51000)) {
   fill(45, 83, 159);
   rect(0, 0, ancho3, largo3);
   }
   
-  if(millis() > 46200 && millis() < 51000) {
+  if(millis() > (comienzo + 46200) && millis() < (comienzo + 51000)) {
   noStroke();
   fill(35, 73, 149);
   ellipse(200, 200, ancho3, largo3);
   } 
   
-  if(millis() > 46800 && millis() < 51000) {
+  if(millis() > (comienzo + 46800) && millis() < (comienzo + 51000)) {
   noStroke();
   fill(25, 63, 139);
   triangle(200, 0, 400, 400, 0, 400);
   }
   
-  if(millis() > 47600 && millis() < 51000) {
+  if(millis() > (comienzo + 47600) && millis() < (comienzo + 51000)) {
   noStroke()
   fill(15, 53, 129);
   beginShape();
@@ -518,24 +538,24 @@ function draw() {
   endShape(CLOSE);
   }
   
-  if(millis() > 48200 && millis() < 51000) {
+  if(millis() > (comienzo + 48200) && millis() < (comienzo + 51000)) {
   fill(5, 43, 119);
   rect(100, 100, 200, 200);
   }
   
-  if(millis() > 48800 && millis() < 51000) {
+  if(millis() > (comienzo + 48800) && millis() < (comienzo + 51000)) {
   noStroke();
   fill(0, 33, 109);
   ellipse(200, 200, 200, 200);
   }
  
-  if(millis() > 49400 && millis() < 51000) {
+  if(millis() > (comienzo + 49400) && millis() < (comienzo + 51000)) {
   noStroke();
   fill(0, 23, 99);
   triangle(200, 100, 300, 250, 100, 250);
   }
   
-  if(millis() > 50000 && millis() < 51000) {
+  if(millis() > (comienzo + 50200) && millis() < (comienzo + 51000)) {
   noStroke()
   fill(0, 13, 89);
   beginShape();
@@ -545,6 +565,5 @@ function draw() {
   vertex(100, 200);
   endShape(CLOSE);
   }
-
-    
+  
 }
