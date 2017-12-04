73aa127e066c58765b25a4f3d822675974e2b24c
cestrada14 <31419438+cestrada14@users.noreply.github.com>
2017-12-04 14:14:54 -0500

    Update sketch.js

--- 10/sketch.js
+++ 10/sketch.js
@@ -157,8 +157,8 @@ function draw() {
       
 }
 
-function keyReleased() { // si suelta alguna tecla cambia de estado
-//function deviceShaken() {
+//function keyReleased() { // si suelta alguna tecla cambia de estado
+function deviceShaken() {
 
   if (estado == INTRO) {
     //if (keyCode == ENTER) {
